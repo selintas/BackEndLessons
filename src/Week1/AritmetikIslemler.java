3a15de77a41121b262836d409542bc17d4f3449f
selintas <sselintas@gmail.com>
2024-04-27 22:56:30 +0300

    week1 arit deneme1

--- src/Week1/AritmetikIslemler.java
+++ src/Week1/AritmetikIslemler.java
@@ -6,7 +6,8 @@ public class AritmetikIslemler {
 
     public static void main(String[] args) {
 
-        // a, b, c için a+b*c-b. hesabı yapalım.
+        // a, b, c için a+b*c-b. hesabı yapalım....
+
 
         //  10, 2 ve 3 olsun. Yapılacak işlem ise 10+2*3-2 şeklinde olsun.
         Scanner inp = new Scanner(System.in);
