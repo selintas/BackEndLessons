e16592cf0d131db1a31fb5b55b34ee93d7392dc3
selintas <sselintas@gmail.com>
2024-06-28 23:34:56 +0300

    Recursive Methods / Palindrome Number week2 / technical part of 3 / Classes - StudentNoteSystem

---
+++ src/week2/teknik3/StudentNoteSystem/Teacher.java
@@ -0,0 +1,13 @@
+package week2.teknik3.StudentNoteSystem;
+
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
